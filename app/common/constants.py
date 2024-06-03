6018521d33d9f88ef10512a445142c8c1532211c
ayrton <ayrtoncoelhods@gmail.com>
2024-06-03 14:31:30 -0300

    app constants

---
+++ app/common/constants.py
@@ -0,0 +1,14 @@
+class App:
+    name = "APP_NAME"
+    host = "APP_HOST"
+    port = "APP_PORT"
+
+
+class MongoDB:
+    uri = "MONGODB_URI"
+    db = "MONGODB_DB"
+
+
+class Env:
+    APP = App()
+    MONGODB = MongoDB()
